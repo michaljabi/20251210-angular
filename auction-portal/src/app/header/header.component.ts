59c7bfd9e25307b774bfe64996b9edde592c6bc7
Jabi <michal.jabi@gmail.com>
2025-12-10 14:50:22 +0100

    task(8.4): use signal() to change value in time

--- auction-portal/src/app/header/header.component.ts
+++ auction-portal/src/app/header/header.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   template: `
     <header class="mb-2 p-5 bg-warning">
-      <h1>Auction portal</h1>
+      <h1>{{ title() }}</h1>
+      {{ title() }}
     </header>
   `,
   styles: ``,
 })
 export class HeaderComponent {
+  protected readonly title = signal('Auction portal');
 
   constructor() {
+    //this.title = 'hello'
+    console.log('hello ?');
     setTimeout(() => {
-      
-    }, 4000)
+      // this.title = 'Auction portal!'
+
+      // Rozwiązania:
+      // this.title.set('Auction portal!');
+      // this.title.set(this.title() + '!');
+      this.title.update((t) => t + '!');
+    }, 4000);
   }
 }
