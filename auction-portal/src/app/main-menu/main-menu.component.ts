0cbb618099133d97e39862a50763cdb1bc22ed41
Jabi <michal.jabi@gmail.com>
2025-12-10 16:00:40 +0100

    task(10): main-menu works + burger menu working

--- auction-portal/src/app/main-menu/main-menu.component.ts
+++ auction-portal/src/app/main-menu/main-menu.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
   imports: [],
   template: `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 mb-3">
-      <button class="navbar-toggler" type="button">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 mb-3" [title]="myTooltip" >
+      <button class="navbar-toggler" type="button" (click)="handleMenuToggle()">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse show">
+      <!-- <div class="collapse navbar-collapse" [class.show]="true"> -->
+      <!-- <div class="collapse navbar-collapse" [ngClass]="{ 'show': true }"> -->
+      <div class="collapse navbar-collapse" [class]="{ 'show': isMenuOpen }">
         <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link" href="/auctions">Aukcje</a>
@@ -25,4 +27,16 @@ import { Component } from '@angular/core';
   `,
   styles: ``,
 })
-export class MainMenuComponent {}
+export class MainMenuComponent {
+
+  myTooltip = 'Hello!'
+  auctions = 'Aukcje'
+
+  isMenuOpen = false;
+  // isMenuOpenSn = signal(false);
+
+  handleMenuToggle() {
+    this.isMenuOpen = !this.isMenuOpen;
+    // this.isMenuOpenSn.set(!this.isMenuOpenSn())
+  }
+}
