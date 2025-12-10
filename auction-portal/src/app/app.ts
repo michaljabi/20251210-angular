01977eb65b937dc06ba6d89f8a144e3ce03bfb99
Jabi <michal.jabi@gmail.com>
2025-12-10 12:24:56 +0100

    explain: data binding + signals initial.

--- auction-portal/src/app/app.ts
+++ auction-portal/src/app/app.ts
@@ -1,13 +1,34 @@
+import { JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   // template: `Hello World`,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('auction-portal');
+  // Dane które nie zmieniają się w czasie
+  protected readonly title = 'auction-portal';
+  // Stan danych
+  protected readonly hello = signal('Hello World');
+  protected readonly auctions = signal([{ name: 'Aukcja' }]);
+
+  constructor() {
+    // @ts-ignore
+    // console.log(x);
+    setTimeout(() => {
+      this.hello.set('Goodbye world');
+      this.auctions.update((current) => [...current, { name: 'Kolejna...' }]);
+    }, 2000);
+
+    setTimeout(() => {
+       this.auctions.update((current) => current.map(a => {
+        a.name = 'HELLO';
+        return a
+      }));
+    }, 5000)
+  }
 }
