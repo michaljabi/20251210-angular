d218a48d9e8f0ba99eb9b0ba84deefd0e65f5a2a
Jabi <michal.jabi@gmail.com>
2025-12-11 11:44:52 +0100

    explain(app.routes): using redirectTo -> '' -> '/auctions'

--- auction-portal/src/app/app.routes.ts
+++ auction-portal/src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { AuctionsPageComponent } from './auctions/auctions-page.component';
 
 export const routes: Routes = [
-    { path: 'auctions', component: AuctionsPageComponent }
+    //{ path: '', component: AuctionsPageComponent },
+  { path: '', redirectTo: '/auctions', pathMatch: 'full' },
+  { path: 'auctions', component: AuctionsPageComponent },
 ];
