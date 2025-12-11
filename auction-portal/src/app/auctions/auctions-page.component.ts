252a1f3fe1203363b2d646de5f67000330e1c2c6
Jabi <michal.jabi@gmail.com>
2025-12-11 15:00:46 +0100

    task(17): make it work: httpClient + auctions-resource service - Dependency Injection.

--- auction-portal/src/app/auctions/auctions-page.component.ts
+++ auction-portal/src/app/auctions/auctions-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AuctionItem } from './auction-item';
+import { AuctionsResourceService } from './auctions-resource.service';
 
 @Component({
   selector: 'app-auctions-page',
@@ -9,25 +10,31 @@ import { AuctionItem } from './auction-item';
       <h2>Lista naszych aukcji</h2>
       <div class="row">
         @for(i of [1,2,3,4,5,6,7,8,9]; track i) {
-          <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">[aukcja {{ i }}]</div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">[aukcja {{ i }}]</div>
         } ...
       </div>
     </section>
   `,
   styles: ``,
+  // providers: [AuctionsResourceService]
 })
 export class AuctionsPageComponent implements OnInit {
-
   //https://angular.dev/api/core/inject#usage-notes
   //engine = inject(Engine);
+  private readonly auctionsResourceService = inject(AuctionsResourceService);
 
   // todo: change to signal<AuctionItem[]>([])
-  acutions: AuctionItem[] = []
+  acutions: AuctionItem[] = [];
 
   ngOnInit(): void {
     // Todo: backend call..
 
     // odniesienie do instancji z DependencyInjection
     //this.engine
+    this.auctionsResourceService.getAll().subscribe({
+      next: (auctionsFromServer) => {
+        console.log(auctionsFromServer);
+      },
+    });
   }
 }
--- auction-portal/src/app/auctions/auctions-resource.service.ts
+++ auction-portal/src/app/auctions/auctions-resource.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { AuctionItem } from './auction-item';
+import { HttpClient } from '@angular/common/http';
 
+// to jest TZW. stateless service
+// to znaczy że nie przechowuje (cacheuje) żadnych danych  w odróżnieniu do statefull.
 @Injectable({
   providedIn: 'root',
 })
 export class AuctionsResourceService {
-  
+  private readonly auctionsEndpoint = 'http://localhost:3000/auctions';
+  private readonly httpClient = inject(HttpClient);
+
+  getAll() {
+    return this.httpClient.get<AuctionItem[]>(this.auctionsEndpoint);
+  }
+
+  // Zaszyta logika do odpytania tylko auckji z promocjami:
+  // http://localhost:3000/auctions?isPromoted=true
+  getAllPromotions() {
+    return this.httpClient.get<AuctionItem[]>(this.auctionsEndpoint, {
+      params: { isPromoted: true },
+    });
+  }
+
+  addOne(auction: AuctionItem) {
+    return this.httpClient.post<AuctionItem>(this.auctionsEndpoint, auction);
+  }
 }
