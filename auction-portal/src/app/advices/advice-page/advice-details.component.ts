18639b62ab7fbce5ad26d4ca2804b3a22b098e73
Jabi <michal.jabi@gmail.com>
2025-12-12 13:04:06 +0100

    explain: use Obeserable for params in routing - to assure always fresh value is used.

--- auction-portal/src/app/advices/advice-page/advice-details.component.ts
+++ auction-portal/src/app/advices/advice-page/advice-details.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-advice-details',
-  imports: [],
+  imports: [JsonPipe],
   template: `
-    <p>
-      advice-details works!
-    </p>
+    <div>
+      {{ activatedRoute.snapshot.params | json }}
+    </div>
   `,
   styles: ``,
 })
-export class AdviceDetailsComponent {
+export class AdviceDetailsComponent implements OnInit {
+    protected readonly activatedRoute = inject(ActivatedRoute);
 
+    ngOnInit(): void {
+      // to będzie jednorazowe, jeśli spełnimy warunek :adviceId w routing table!
+      // console.log(this.activatedRoute.snapshot.params)
+
+      // Dlatego to jest bezpieczniejsze !
+      this.activatedRoute.params.subscribe((params) => {
+        // console.log(params);
+        this.loadAdviceByIdFormBackend(Number(params['adviceId']))
+      })
+    }
+
+    loadAdviceByIdFormBackend(id: number) {
+      console.log(id);
+    }
 }
--- auction-portal/src/app/advices/advices.routes.ts
+++ auction-portal/src/app/advices/advices.routes.ts
@@ -1,7 +1,14 @@
-import { Routes } from "@angular/router";
-import { AdvicePageComponent } from "./advice-page/advice-page.component";
-
+import { Routes } from '@angular/router';
+import { AdvicePageComponent } from './advice-page/advice-page.component';
+import { AdviceDetailsComponent } from './advice-page/advice-details.component';
 
 export const routes: Routes = [
-    { path: '', component: AdvicePageComponent },
-]
+  {
+    path: '',
+    component: AdvicePageComponent,
+    children: [
+        { path: '', component: AdviceDetailsComponent },
+        { path: ':adviceId', component: AdviceDetailsComponent },
+    ],
+  },
+];
