07c60b8cf3972fa9429e51873e7d8e1494f5eec1
Jabi <michal.jabi@gmail.com>
2025-12-12 12:04:24 +0100

    explain: refactor to features: /auctions /advices /common

--- auction-portal/src/app/app.ts
+++ auction-portal/src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { MainMenuComponent } from "./main-menu/main-menu.component";
+import { HeaderComponent } from "./common/header/header.component";
+import { MainMenuComponent } from "./common/main-menu/main-menu.component";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, MainMenuComponent],
   templateUrl: './app.html',
   // template: `Hello World`,
-  styleUrl: './app.css',
 })
 export class App {
   // Dane które nie zmieniają się w czasie
