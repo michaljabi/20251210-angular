61c07e4a2bf860c270efc9acc517eeafa23eb115
Jabi <michal.jabi@gmail.com>
2025-12-12 15:13:48 +0100

    initial: add-auction form.

--- auction-portal/src/app/auctions/add-auction-page/add-auction-page.component.ts
+++ auction-portal/src/app/auctions/add-auction-page/add-auction-page.component.ts
@@ -4,12 +4,74 @@ import { Component } from '@angular/core';
   selector: 'app-add-auction-page',
   imports: [],
   template: `
-     <section>
+    <div>
       <h2>Dodaj nową aukcję</h2>
-     </section>
+      <section class="mt-2 row">
+        <div class="col-6">
+          <img
+            class="img-thumbnail"
+            alt="Podgląd fotografii"
+            [src]="'https://picsum.photos/id/1/600/600'"
+          />
+        </div>
+        <div class="col-6">
+          <form>
+            <div class="form-group">
+              <label for="auctionTitle">Nazwa aukcji</label>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i icon="edit"></i>
+                  </span>
+                </div>
+                <input id="auctionTitle" type="text" name="title" required class="form-control" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="auctionPrice">Cena aukcji</label>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i icon="tag"></i>
+                  </span>
+                </div>
+                <input id="auctionPrice" type="number" name="price" required class="form-control" />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="img">Zdjecie</label>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i icon="image"></i>
+                  </span>
+                </div>
+                <input id="img" type="number" name="imgUrl" required class="form-control" />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="auctionDescription">Szczegółowy opis</label>
+              <div class="input-group mb-3">
+                <textarea
+                  id="auctionDescription"
+                  rows="5"
+                  class="form-control"
+                  name="description"
+                ></textarea>
+              </div>
+            </div>
+            <div class="text-right">
+              <button class="btn btn-primary" type="submit">
+                <i icon="gavel"></i> Dodaj aukcję
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
   `,
   styles: ``,
 })
-export class AddAuctionPageComponent {
-
-}
+export class AddAuctionPageComponent {}
