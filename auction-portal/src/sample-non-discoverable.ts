a4758f00aaebb81f80d4b32f7e62bf0161d5f1f3
Jabi <michal.jabi@gmail.com>
2025-12-12 12:50:36 +0100

    explain: bundler way of work when file is not imported.

--- auction-portal/src/sample-non-discoverable.ts
+++ auction-portal/src/sample-non-discoverable.ts
@@ -1,1 +1,6 @@
+/*
+Dane z tego pliku nie są nigdzie `import` 'owane - co za tym idzie to jest "zombie".
+Ten kod nie dostanie się na produkcję (do produkcyjnego bundle)
+*/
+
 export const myData = { dummy: 'data' };
